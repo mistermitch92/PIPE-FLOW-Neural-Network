01b86eca6457f776972591dc98fce606e75e9855
mistermitch92 <delemee4@msu.edu>
2024-05-22 15:20:15 -0400

    Increased Dataset size and make a Keras model

--- caprheo_theoreticalData.py
+++ caprheo_theoreticalData.py
@@ -9,13 +9,13 @@ import pandas as pd
 from tqdm import tqdm
 
 
-#GIVEN VALUES WE WILL EVENTUALLY TRY TO PREDICT
+#TARGETS WE WILL EVENTUALLY TRY TO PREDICT
 
-yield_stress = np.linspace(0, 2.5E6, num=5) #yield stress (pascals)
-slip_yield_stress = np.linspace(0, 5.0E5, num=5) #slip yield stress (pascals)
-Beta = np.linspace(5E-11, 5E-9, num=5) #slip velocity coefficient
-thinning_index = np.linspace(0.6, 0.7, num=5) #shear thinning index
-Viscosity_consistency = np.linspace(800, 1300, num=5) #shear thinning consistency (pa.s^n)
+yield_stress = np.linspace(2.5E4, 2.5E5, num=10) #yield stress (pascals)
+slip_yield_stress = np.linspace(5E4, 5E5, num=10) #slip yield stress (pascals)
+Beta = np.linspace(5E-11, 5E-9, num=4) #slip velocity coefficient
+thinning_index = np.linspace(0.5, 0.8, num=4) #shear thinning index
+Viscosity_consistency = np.linspace(800, 1300, num=4) #shear thinning consistency (pa.s^n)
 
 
 #initial conditions:
@@ -37,36 +37,41 @@ print("Total iterations: ", iterations)
 
 #empty array to store the data
 stored_theory_values = np.array([])
-stored_given_values = np.array([])
+stored_target_values = np.array([])
 
 
 #calculate the apparent wall shear rate (gamma_dot_apparent) for each case
-for ty in tqdm(yield_stress):
-    for tys in slip_yield_stress:
-        for beta in Beta:
-            for n in thinning_index:
-                s = 1/n #shear thinning index reciprocal
-                for k in Viscosity_consistency:
-                    for ii in range(len(Radius)):
-                        for g_i in G:
-                            #pressure drop for each Radius/Aspect Ratio pair and the given pressure gradient (Pa)
-                            dP = g_i*Length[ii]
-                        
-                            #wall shear stress (Pa)
-                            tw = g_i*Radius[ii]/2
+for g_i in tqdm(G):
+     for ii in range(len(Radius)):
 
-                            #Radius of plug (meters)
-                            ri = Radius[ii]*ty/tw
+        #pressure drop for each Radius/Aspect Ratio pair and the pressure gradient (Pa)
+        dP = g_i*Length[ii]
 
-                            #thickness of slip layer (meters)
-                            delta = Radius[ii]*tys/tw
+        #wall shear stress (Pa)
+        tw = g_i*Radius[ii]/2
 
-                            #slip velocity (m/s)
-                            if tw > tys:
-                                vs0 = beta*tw
-                            else:
-                                vs0 = 0
 
+        for ty in yield_stress:
+            #Radius of plug (meters)
+            ri = Radius[ii]*ty/tw
+
+            for tys in slip_yield_stress:
+                #thickness of slip layer (meters)
+                delta = Radius[ii]*tys/tw
+
+                for beta in Beta:
+                    #slip velocity (m/s)
+                    if tw > tys:
+                        vs0 = beta*tw
+                    else:
+                        vs0 = 0
+
+                    for n in thinning_index:
+                        #shear thinning index reciprocal
+                        s = 1/n 
+
+                        for k in Viscosity_consistency:
+                            
                             #plug velocity (m/s)
                             if tw > ty:
                                 v0 = (g_i*(Radius[ii]-ri)**(s+1))/(2*k*(s+1))
@@ -87,13 +92,13 @@ for ty in tqdm(yield_stress):
 
                             temp_theorydata = np.array([Radius[ii], aspectRatio[ii], dP, g_i, gamma_dot_apparent, Qtot])
 
-                            temp_givendata = np.array([ty, tys, beta, n, k])
+                            temp_target_data = np.array([beta, n, k, Qslip, Qplug])
 
                             #store the theoretical data from each iteration in a matrix (turn into a pandas dataframe later)
                             stored_theory_values = np.concatenate((stored_theory_values, temp_theorydata), axis = 0)
 
-                            #store the given data from each iteration in a matrix (turn into a pandas dataframe later)
-                            stored_given_values = np.concatenate((stored_given_values, temp_givendata), axis = 0)
+                            #store the target values from each iteration in a matrix (turn into a pandas dataframe later)
+                            stored_target_values = np.concatenate((stored_target_values, temp_target_data), axis = 0)
 
 
 
@@ -107,11 +112,11 @@ Theory_Data = pd.DataFrame(stored_theory_values, columns = ['Radius (m)', 'Aspec
 #save the data to a csv file
 Theory_Data.to_csv('Theory_Data.csv', index = False)
 
-#reshape the given data into a matrix
-stored_given_values = np.reshape(stored_given_values, (iterations, 5))
+#reshape the target data into a matrix
+stored_target_values = np.reshape(stored_target_values, (iterations, 5))
 
 #convert the stored data into a pandas dataframe
-Given_Data = pd.DataFrame(stored_given_values, columns = ['Yield Stress (Pa)', 'Slip Yield Stress (Pa)', 'Slip Velocity Coefficient', 'Shear Thinning Index', 'Shear Thinning Consistency (Pa s^n)'])
+Target_Data = pd.DataFrame(stored_target_values, columns = ['Slip Velocity Coefficient', 'Shear Thinning Index', 'Shear Thinning Consistency (Pa s^n)', 'Slip Volumetric Flow Rate (m^3 s^-1)', 'Plug Volumetric Flow Rate (m^3 s^-1)'])
 
 #save the data to a csv file
-Given_Data.to_csv('Given_Data.csv', index = False)
+Target_Data.to_csv('Target_Data.csv', index = False)
